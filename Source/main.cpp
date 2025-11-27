cb2d329ba2e028e66419391d8d5b4892bc4558b7
AhmedYasser-cell <madoyasser2@gmail.com>
2025-11-27 14:09:22 +0200

    Fix Implementation And Linker Bug

--- Source/main.cpp
+++ Source/main.cpp
@@ -18,9 +18,9 @@ using namespace std;
 
 void RunInfinityXO()
 {
-    UI<char>* game_ui = new XO_UI();
+    UI<char>* game_ui = new inf_XO_UI();
 
-    Board<char>* xo_board = new X_O_Board();
+    Board<char>* xo_board = new inf_X_O_Board();
 
     Player<char>** players = game_ui->setup_players();
 
@@ -40,9 +40,9 @@ void RunInfinityXO()
 
 void Run5x5XO()
 {
-    UI<char>* game_ui = new XO_UI();
+    UI<char>* game_ui = new _5X5_XO_UI();
 
-    Board<char>* xo_board = new X_O_Board();
+    Board<char>* xo_board = new _5X5_X_O_Board();
 
     Player<char>** players = game_ui->setup_players();
 
